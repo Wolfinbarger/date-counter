25bb91d32aa4d165fd6be536cf0709829b6f18b9
XZaiver Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-08-02 20:53:42 -0500

    created text and buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { useState } from "react";
+
 function App() {
-  return <div>Test</div>;
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
 }
 
 export default App;
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setcount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <button>-</button>
+        Step: {step}
+        <button>+</button>
+      </div>
+      <div>
+        <button>-</button>
+        Count: {count}
+        <button>+</button>
+      </div>
+      <div>{count} days from today is</div>
+    </>
+  );
+}
